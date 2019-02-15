e8e7edaa9651c0e9e460a00dbea6570b8bd6996a
Bien Cueto <you@example.com>
2019-02-15 23:47:34 +0400

    sections

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var gulp = require("gulp"),
 var paths = {
     styles: {
         // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-        src: ['scss/*.scss'],
+        src: ['scss/*.scss','scss/**/*.scss'],
         // Compiled files will end up in whichever folder it's found in (partials are not compiled)
         dest: "css/"
     }
